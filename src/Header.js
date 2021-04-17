dd6e3857c2a17ce96bfa7fca21714e0a77b94582
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-17 01:10:24 -0600

    put in roboto font and changed header to brown

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import AcUnitRoundedIcon from "@material-ui/icons/AcUnitRounded";
+import PetsIcon from "@material-ui/icons/Pets";
 import { makeStyles } from "@material-ui/styles";
+import brown from "@material-ui/core/colors/brown";
 
 const useStyles = makeStyles(() => ({
 	appBar: {
 		marginBottom: "1rem",
+		backgroundColor: brown[500],
 	},
 	typographyStyles: {
 		flex: 1,
@@ -18,10 +20,10 @@ export default function Header() {
 	return (
 		<AppBar position="static" className={classes.appBar}>
 			<Toolbar>
-				<Typography className={classes.typographyStyles}>
-					This is the header
+				<Typography variant="h4" className={classes.typographyStyles}>
+					Fun Dog Toys
 				</Typography>
-				<AcUnitRoundedIcon />
+				<PetsIcon />
 			</Toolbar>
 		</AppBar>
 	);
