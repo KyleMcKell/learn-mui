a7da9c0297bace6b695c8043acac5e5fb5ef90ba
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-17 00:29:58 -0600

    added uuid and added in a card component to work with

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import data from "data";
+import DogToyCard from "DogToyCard";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Content() {
 	return (
 		<Grid container spacing={4}>
-			<Grid item xs={12} sm={6} md={4}></Grid>
-			<Grid item xs={12} sm={6} md={4}></Grid>
-			<Grid item xs={12} sm={6} md={4}></Grid>
+			{data.map((toy) => {
+				return (
+					<Grid item xs={12} sm={6} md={4}>
+						<DogToyCard toy={toy} key={uuidv4()} />
+					</Grid>
+				);
+			})}
 		</Grid>
 	);
 }
---
+++ src/DogToyCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DogToyCard() {
+	return (
+		<div>
+			<h1>This is a dog toy card</h1>
+		</div>
+	);
+}
