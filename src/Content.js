023e695678c5a87887d6232baa2280b75272c1a7
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-16 20:32:36 -0600

    got rid of coffeecard

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import CoffeeCard from "CoffeeCard";
 
 export default function Content() {
 	return (
 		<Grid container spacing={4}>
-			<Grid item xs={12} sm={6} md={4}>
-				<CoffeeCard />
-			</Grid>
-			<Grid item xs={12} sm={6} md={4}>
-				<CoffeeCard />
-			</Grid>
-			<Grid item xs={12} sm={6} md={4}>
-				<CoffeeCard />
-			</Grid>
+			<Grid item xs={12} sm={6} md={4}></Grid>
+			<Grid item xs={12} sm={6} md={4}></Grid>
+			<Grid item xs={12} sm={6} md={4}></Grid>
 		</Grid>
 	);
 }
