795fbf43bc97cfe3d051dd98b1628a5b9d4a4679
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-16 20:42:00 -0600

    added some mock data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+	{
+		title: "No Stuffing Squeaky Plush Dog Toy",
+		price: 10.99,
+		description:
+			"ZippyPaws Skinny Peltz are no-stuffing plush dog toys that provide hours of squeaking fun! This set includes 3 large toys (1 of each: Fox, Raccoon, and Squirrel). Each toy includes 2 round squeakers.",
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/712YaF31-3L._AC_SL1000_.jpg",
+	},
+	{
+		title: "18 Piece Dog Chew Toys",
+		price: 23.99,
+		description:
+			"18 PERFECT COMBINATION OF DOG TOYS FOR TUG, CHEWING, AND SHAKING – large dog toys that withstand playtime with large dogs and these tough dog toys are great for large breeds and fetch.",
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/71jfi9AMvSL._AC_SL1100_.jpg",
+	},
+	{
+		title: "Lambchop Plush Dog Toy with Squeaker",
+		price: 5.99,
+		description:
+			"Nostalgic for you and big, adorable fun for your dog, Lamb Chop will be an instant favorite. Whether your dog is into fetch or happily squeaking the toy, this beloved character brings along instant joy.",
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/81O7mf19NeL._AC_SL1500_.jpg",
+	},
+];
+
+export default data;
